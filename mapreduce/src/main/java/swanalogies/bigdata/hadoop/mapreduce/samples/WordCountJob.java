8dc5c3a3285d60f65e58083983465bdfa0045a4c
Rishi Jariwala <rj@Rishis-MacBook-Pro.local>
2018-05-21 12:43:31 -0400

    Committing local state

---
+++ mapreduce/src/main/java/swanalogies/bigdata/hadoop/mapreduce/samples/WordCountJob.java
@@ -0,0 +1,33 @@
+package swanalogies.bigdata.hadoop.mapreduce.samples;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import swanalogies.bigdata.hadoop.mapreduce.samples.mappers.WordCountMapper;
+import swanalogies.bigdata.hadoop.mapreduce.samples.reducers.WordCountReducer;
+
+public class WordCountJob {
+    public static void main(String[] args) throws Exception {
+        //entry point for the job
+        configureJob(args);
+    }
+
+    private static void configureJob(final String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "wordcountsample");
+        job.setJarByClass(WordCountJob.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ mapreduce/src/main/java/swanalogies/bigdata/hadoop/mapreduce/samples/mappers/WordCountMapper.java
@@ -0,0 +1,23 @@
+package swanalogies.bigdata.hadoop.mapreduce.samples.mappers;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer stringTokenizer = new StringTokenizer(value.toString());
+
+        while (stringTokenizer.hasMoreTokens()) {
+            word.set(stringTokenizer.nextToken());
+            context.write(word, one);
+        }
+    }
+}
